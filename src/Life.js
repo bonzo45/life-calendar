a8f13350a02da792dda5e7ed2904cb4280223b1c
Sam Esgate <sam.esgate@sparx.co.uk>
2020-04-05 10:01:56 +0100

    Simplify with switch statement

--- src/Life.js
+++ src/Life.js
@@ -3,84 +3,91 @@ import { gsap } from 'gsap';
 import './Life.css';
 
 function Life() {
-    const [ state, setState ] = useState('start');
+    const [ state, setState ] = useState(0);
 
     const prepareAnimation = () => {
         const timeline = gsap.timeline();
         timeline.to('.LifeTitleWrapper, .LifeCalendar, .MonthHeading.BirthMonth, .Month.BirthMonth, .MonthHeading:not(.BirthMonth), .Month:not(.BirthMonth)', {opacity: 0, duration: 0});
     };
 
+    const fadeIn = duration => ({autoAlpha: 1, duration});
+
     const runAnimation = () => {
         const timeline = gsap.timeline();
 
-        if (state === 'start') {
-            timeline.addLabel('start');
-            const arrowAnimation = {y: '-300px', duration: 0.5};
-            const arrowAnimationTo = {autoAlpha: 1, duration: 0.5};
-            timeline.from('.Continue', arrowAnimation, 'start');
-            timeline.to('.Continue', arrowAnimationTo, 'start');
-            setState('title');
-        } else if (state === 'title') {
-            timeline.addLabel('title');
-            const titleAnimation = {y: '-100px', duration: 1.25};
-            const titleAnimationTo = {opacity: 1, duration: 1.25};
-            timeline.from('.LifeTitleWrapper', titleAnimation, 'title');
-            timeline.to('.LifeTitleWrapper', titleAnimationTo, 'title');
-            setState('birth');
-        } else if (state === 'birth') {
-            timeline.from('.LifeCalendar', {scale: 0.95, duration: 0.5}, '-=0.25');
-            timeline.to('.LifeCalendar', {opacity: 1, duration: 0.5}, '-=0.25');
+        switch (state) {
+            default:
+            case 0:
+                timeline.addLabel('start');
+                const buttonDuration = 0.5;
+                timeline.from('.Continue', {y: '-300px', duration: buttonDuration}, 'start');
+                timeline.to('.Continue', fadeIn(buttonDuration), 'start');
+                break;
 
-            timeline.addLabel('Birth');
-            const birthDuration = 0.5;
-            const birthStagger = 0.5;
+            case 1:
+                timeline.addLabel('title');
+                const titleAnimation = {y: '-100px', duration: 1.25};
+                const titleAnimationTo = {opacity: 1, duration: 1.25};
+                timeline.from('.LifeTitleWrapper', titleAnimation, 'title');
+                timeline.to('.LifeTitleWrapper', titleAnimationTo, 'title');
+                break;
 
-            const monthHeadingAnimation = {y: '-18px', duration: birthDuration, stagger: birthStagger};
-            const monthHeadingAnimationTo = {opacity: 1, duration: birthDuration, stagger: birthStagger};
-            timeline.from('.MonthHeading.BirthMonth', monthHeadingAnimation, 'Birth');
-            timeline.to('.MonthHeading.BirthMonth', monthHeadingAnimationTo, 'Birth');
+            case 2:
+                timeline.from('.LifeCalendar', {scale: 0.95, duration: 0.5}, '-=0.25');
+                timeline.to('.LifeCalendar', {opacity: 1, duration: 0.5}, '-=0.25');
 
-            const birthAnimation = {x: '-40px', duration: birthDuration, stagger: birthStagger};
-            const birthAnimationTo = {opacity: 1, duration: birthDuration, stagger: birthStagger};
-            timeline.from('.Month.BirthMonth', birthAnimation, 'Birth');
-            timeline.to('.Month.BirthMonth', birthAnimationTo, 'Birth');
+                timeline.addLabel('Birth');
+                const birthDuration = 0.5;
+                const birthStagger = 0.5;
 
-            const birthTutorialAnimation = {x: '-40px', duration: birthDuration};
-            const birthTutorialAnimationTo = {opacity: 1, duration: birthDuration};
-            timeline.from('.Month.BirthMonth > .Tutorial', birthTutorialAnimation);
-            timeline.to('.Month.BirthMonth > .Tutorial', birthTutorialAnimationTo);
+                const monthHeadingAnimation = {y: '-18px', duration: birthDuration, stagger: birthStagger};
+                const monthHeadingAnimationTo = {opacity: 1, duration: birthDuration, stagger: birthStagger};
+                timeline.from('.MonthHeading.BirthMonth', monthHeadingAnimation, 'Birth');
+                timeline.to('.MonthHeading.BirthMonth', monthHeadingAnimationTo, 'Birth');
 
-            setState('year1');
-        } else if (state === 'year1') {
-            timeline.addLabel('Year 1');
-            const year1Duration = 0.5;
-            const year1Stagger = 0.25;
-            const monthHeadingAnimation = {y: '-18px', duration: year1Duration, stagger: year1Stagger};
-            const monthHeadingAnimationTo = {opacity: 1, duration: year1Duration, stagger: year1Stagger};
-            timeline.from('.MonthHeading:not(.BirthMonth)', monthHeadingAnimation, 'Year 1');
-            timeline.to('.MonthHeading:not(.BirthMonth)', monthHeadingAnimationTo, 'Year 1');
+                const birthAnimation = {x: '-40px', duration: birthDuration, stagger: birthStagger};
+                const birthAnimationTo = {opacity: 1, duration: birthDuration, stagger: birthStagger};
+                timeline.from('.Month.BirthMonth', birthAnimation, 'Birth');
+                timeline.to('.Month.BirthMonth', birthAnimationTo, 'Birth');
 
-            const year1Animation = {opacity: 0, x: '-40px', duration: year1Duration, stagger: year1Stagger};
-            const year1AnimationTo = {opacity: 1, duration: year1Duration, stagger: year1Stagger};
-            timeline.from('.Year1 > .Month:not(.BirthMonth)', year1Animation, 'Year 1');
-            timeline.to('.Year1 > .Month:not(.BirthMonth)', year1AnimationTo, 'Year 1');
+                const birthTutorialAnimation = {x: '-40px', duration: birthDuration};
+                const birthTutorialAnimationTo = {opacity: 1, duration: birthDuration};
+                timeline.from('.Month.BirthMonth > .Tutorial', birthTutorialAnimation);
+                timeline.to('.Month.BirthMonth > .Tutorial', birthTutorialAnimationTo);
+                break;
 
-            setState('restoflife');
-        } else if (state === 'restoflife') {
-            timeline.addLabel('Rest of Life');
-            const restOfLifeDuration = 0.25;
-            const restOfLifeStagger = 0.005;
-            const restOfLifeAnimation = {opacity: 0, x: '-40px', duration: restOfLifeDuration, stagger: restOfLifeStagger};
-            const restOfLifeAnimationTo = {opacity: 1, duration: restOfLifeDuration, stagger: restOfLifeStagger};
-            timeline.from('.Year:not(.Year1) > .Month', restOfLifeAnimation, 'Year 1');
-            timeline.to('.Year:not(.Year1) > .Month', restOfLifeAnimationTo, 'Year 1');
+            case 3:
+                timeline.addLabel('Year 1');
+                const year1Duration = 0.5;
+                const year1Stagger = 0.25;
+                const yearHeadingAnimation = {y: '-18px', duration: year1Duration, stagger: year1Stagger};
+                const yearHeadingAnimationTo = {opacity: 1, duration: year1Duration, stagger: year1Stagger};
+                timeline.from('.MonthHeading:not(.BirthMonth)', yearHeadingAnimation, 'Year 1');
+                timeline.to('.MonthHeading:not(.BirthMonth)', yearHeadingAnimationTo, 'Year 1');
 
-            const ninetyDuration = 0.5;
-            const ninetyTutorialAnimation = {x: '40px', duration: ninetyDuration};
-            const ninetyTutorialAnimationTo = {opacity: 1, duration: ninetyDuration};
-            timeline.from('.Month.Ninety > .Tutorial', ninetyTutorialAnimation);
-            timeline.to('.Month.Ninety > .Tutorial', ninetyTutorialAnimationTo);
+                const year1Animation = {opacity: 0, x: '-40px', duration: year1Duration, stagger: year1Stagger};
+                const year1AnimationTo = {opacity: 1, duration: year1Duration, stagger: year1Stagger};
+                timeline.from('.Year1 > .Month:not(.BirthMonth)', year1Animation, 'Year 1');
+                timeline.to('.Year1 > .Month:not(.BirthMonth)', year1AnimationTo, 'Year 1');
+                break;
+
+            case 4:
+                timeline.addLabel('Rest of Life');
+                const restOfLifeDuration = 0.25;
+                const restOfLifeStagger = 0.005;
+                const restOfLifeAnimation = {opacity: 0, x: '-40px', duration: restOfLifeDuration, stagger: restOfLifeStagger};
+                const restOfLifeAnimationTo = {opacity: 1, duration: restOfLifeDuration, stagger: restOfLifeStagger};
+                timeline.from('.Year:not(.Year1) > .Month', restOfLifeAnimation, 'Year 1');
+                timeline.to('.Year:not(.Year1) > .Month', restOfLifeAnimationTo, 'Year 1');
+
+                const ninetyDuration = 0.5;
+                const ninetyTutorialAnimation = {x: '40px', duration: ninetyDuration};
+                const ninetyTutorialAnimationTo = {opacity: 1, duration: ninetyDuration};
+                timeline.from('.Month.Ninety > .Tutorial', ninetyTutorialAnimation);
+                timeline.to('.Month.Ninety > .Tutorial', ninetyTutorialAnimationTo);
+                break;
         }
+        setState(state + 1);
     };
 
     useEffect(prepareAnimation, []);
